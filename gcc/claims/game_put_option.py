614a7cf91fe8a5c7b837a3067f1049a11bec9115
del <daniel@danieleliasson.com>
2012-06-17 21:32:59 +0200

    Fix game put option claim functions

--- gcc/claims/game_put_option.py
+++ gcc/claims/game_put_option.py
@@ -18,7 +18,7 @@ def value(S, K, delta, r, T, **params):
     @param   S:         the simulated underlying paths. L is the number of time steps - 1
                         (timesteps are numbered 0, 1, ..., L), and N is the number of simulated paths,
     @type    K:         number
-    @param   K:         the strike of the call option,
+    @param   K:         the strike of the put option,
     @type    delta:     number or (L+1) x N-array
     @param   delta:     the penalty for terminating the option,
     @type    r:         number
@@ -34,4 +34,7 @@ def value(S, K, delta, r, T, **params):
 
     X = np.maximum(K - S, 0) + delta
     Y = np.maximum(K - S, 0)
+    L = X.shape[0] - 1
+    X[L, :] = Y[L, :]
+
     return valuation.value_gcc(X=X, Y=Y, **params)
